f21566e04e213e815d231b267dbaef3fb99a5ccd
mayahyah <24-02357@g.batstate-u.edu.ph>
2025-11-28 15:39:06 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,52 +1,26 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Player {
-    private Scientists scientist;
     private int score = 0;
+    private int unlockedLevels = 1;
+    private Scientists scientist;
 
-    // unlocked levels 1–20 (index 0 unused)
-    private boolean[] unlocked = new boolean[21];
+    public int getScore() { return score; }
+    public void addScore(int amount) { score += amount; }
 
-    public Player(Scientists scientist) {
-        this.scientist = scientist;
+    public int getUnlockedLevels() { return unlockedLevels; }
 
-        // Only Level 1 unlocked initially (per requirement)
-        unlocked[1] = true;
+    public boolean isUnlocked(int level) {
+        return level <= unlockedLevels;
     }
 
-    public Scientists getScientist() {
-        return scientist;
+    public void unlock(int level) {
+        if (level > unlockedLevels) unlockedLevels = level;
     }
 
     public void setScientist(Scientists s) {
         this.scientist = s;
     }
 
-    public int getScore() {
-        return score;
-    }
-
-    public void addScore(int s) {
-        this.score += s;
-    }
-
-    public boolean isUnlocked(int level) {
-        if (level < 1 || level > 20) return false;
-        return unlocked[level];
-    }
-
-    public void unlock(int level) {
-        if (level >= 1 && level <= 20) {
-            unlocked[level] = true;
-        }
-    }
-
-    public List<Integer> getUnlockedLevels() {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= 20; i++) {
-            if (unlocked[i]) list.add(i);
-        }
-        return list;
+    public Scientists getScientist() {
+        return scientist;
     }
 }
